6fa01bc918f71becb64d1d7abb4dc6f04d660380
DariyaSavva <savvadariya@yandex.ru>
2026-04-27 15:04:38 +0300

    clang format 2

--- llvm/compiler-course/backend/savva_dariya_lab_3/SavvaDariyaInsertCheckPass.cpp
+++ llvm/compiler-course/backend/savva_dariya_lab_3/SavvaDariyaInsertCheckPass.cpp
@@ -17,12 +17,10 @@ public:
   bool runOnMachineFunction(MachineFunction &MF) override;
 
 private:
-  // Проверка на системные регистры
   bool isSystemReg(Register R) const {
     return R == X86::RSP || R == X86::RBP || R == X86::ESP || R == X86::EBP;
   }
 
-  // Поиск базового регистра в инструкции X86
   Register getBaseAddrReg(const MachineInstr &MI) const {
     const MCInstrDesc &Desc = MI.getDesc();
     int MemOp = X86II::getMemoryOperandNo(Desc.TSFlags);
@@ -42,13 +40,10 @@ bool SavvaDariyaInsertCheckPass::runOnMachineFunction(MachineFunction &MF) {
   bool Changed = false;
   const X86InstrInfo *TII = MF.getSubtarget<X86Subtarget>().getInstrInfo();
 
-  // 1. Создаем один общий блок для падения (Trap) на всю функцию
-  // Это экономит место и упрощает CFG.
   MachineBasicBlock *CommonTrapBB = MF.CreateMachineBasicBlock();
   MF.push_back(CommonTrapBB);
   BuildMI(CommonTrapBB, DebugLoc(), TII->get(X86::TRAP));
 
-  // 2. Собираем инструкции, которые требуют проверки
   SmallVector<MachineInstr *, 16> Targets;
   for (auto &MBB : MF) {
     if (&MBB == CommonTrapBB)
@@ -62,35 +57,26 @@ bool SavvaDariyaInsertCheckPass::runOnMachineFunction(MachineFunction &MF) {
     }
   }
 
-  // 3. Модифицируем код
   for (MachineInstr *MI : Targets) {
     MachineBasicBlock *OrigBB = MI->getParent();
     DebugLoc DL = MI->getDebugLoc();
     Register Ptr = getBaseAddrReg(*MI);
 
-    // --- КРИТИЧЕСКОЕ ИСПРАВЛЕНИЕ ТУТ ---
-    // Создаем блок продолжения, куда переедет MI и всё, что за ней
     MachineBasicBlock *ContBB = MF.CreateMachineBasicBlock();
     MF.insert(std::next(OrigBB->getIterator()), ContBB);
 
-    // Переносим "хвост" старого блока в новый
     ContBB->splice(ContBB->end(), OrigBB, MI->getIterator(), OrigBB->end());
 
-    // Переносим связи (Successors) и PHI-ноды
     ContBB->transferSuccessorsAndUpdatePHIs(OrigBB);
 
-    // Теперь вставляем проверку в OrigBB (теперь он заканчивается тут)
     BuildMI(OrigBB, DL, TII->get(X86::TEST64rr)).addReg(Ptr).addReg(Ptr);
 
-    // JE -> CommonTrapBB (если ноль — прыгаем)
     BuildMI(OrigBB, DL, TII->get(X86::JCC_1))
         .addMBB(CommonTrapBB)
         .addImm(X86::COND_E);
 
-    // Добавляем новые связи в CFG
     OrigBB->addSuccessor(CommonTrapBB);
     OrigBB->addSuccessor(ContBB);
-    // -----------------------------------
 
     Changed = true;
   }
